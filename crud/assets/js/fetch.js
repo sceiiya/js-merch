fcbb1389cf15877ea849be2b9983357c84ba0b88
Jayfourth <fourthcesar@gmail.com>
2023-03-25 09:27:13 +0800

    updated modify button

--- crud/assets/js/fetch.js
+++ crud/assets/js/fetch.js
@@ -48,3 +48,62 @@ function del(nId) {
         }
     });
 }
+
+function edit(nId) {
+    var hiddenIndex = document.getElementById("indexer");
+    hiddenIndex.value = nId;
+
+        $.ajax({
+        type: 'POST',
+        url: "controllers/edit-selected.php",
+        data: {nid: nId},
+        success: (result) => {
+            if (result == "error") {
+                alert("Please call system admnistrator");
+            } else {
+                var objRes = JSON.parse(result);
+                sFname = $("#firstName").val(objRes.first_name);
+                sLname = $("#lastName").val(objRes.last_name);
+                sUsername = $("#userName").val(objRes.username);
+                sEmail = $("#emailAddress").val(objRes.email);
+                
+                $('#myModal').modal('show');
+                
+                $("#edit").on('click', () => {
+
+                    var nIndex = $("#indexer").val();
+                    var sFname = $("#firstName").val();
+                    var sLname = $("#lastName").val();
+                    var sEmail = $("#emailAddress").val();
+                    var sUsername = $("#userName").val();
+                    console.log(nIndex);
+                
+                    var sJsonData = {
+                        index: nIndex,
+                        fname: sFname,
+                        lname: sLname,
+                        email: sEmail,
+                        username: sUsername,
+                    }
+                    
+                    $.ajax({
+                        type: 'POST',
+                        url: "controllers/edit-save.php",
+                        data: sJsonData,
+                        success: (result) => {
+                            if (result == "error") {
+                                alert("Please call system admnistrator");
+                            } else {
+                                $('#myModal').modal('hide');
+                                fetch()
+                                
+                            }
+                        }
+                    });
+
+                });
+            }
+        }
+    });
+
+}
