09bcac67e2cbfb0d505b5b52587b3074f534baed
Jayfourth <fourthcesar@gmail.com>
2023-03-25 10:38:52 +0800

    added condition for save.php

--- crud/controllers/save.php
+++ crud/controllers/save.php
@@ -8,24 +8,30 @@
         $sEmail = $_POST['email']; 
         $sUsername = $_POST['username'];
         $sPassword = md5($_POST['password']);
-     
-        try {
-            $qInsert = "INSERT INTO `u955154186_mock_sceiiya`.`tbl_users` 
-                (`first_name`, `last_name`, `email`, `username`, `password`, `date_added`) 
-                VALUES 
-                ('".$sFname."', '{$sLname}', '{$sEmail}', '{$sUsername}', '{$sPassword}', '".date("Y-m-d H:i:s")."')
-            ";
-            
-            $eInsert = mysqli_query($dbConn, $qInsert); //connection and query
+        
+        if( $sFname == "" || $sLname == "" || $sEmail == "" || $sUsername == "" || $sPassword == "" ) {
+            echo "Failed to Process";
+        } else { 
 
-            if ($eInsert == true) {
-                echo "Record successfully saved!";
-            } else {
-                echo "Failed to process, please call system administrator!";
+            try {
+                $qInsert = "INSERT INTO `u955154186_mock_sceiiya`.`tbl_users` 
+                    (`first_name`, `last_name`, `email`, `username`, `password`, `date_added`) 
+                    VALUES 
+                    ('".$sFname."', '{$sLname}', '{$sEmail}', '{$sUsername}', '{$sPassword}', '".date("Y-m-d H:i:s")."')
+                ";
+                
+                $eInsert = mysqli_query($dbConn, $qInsert); //connection and query
+
+                if ($eInsert == true) {
+                    echo "Record successfully saved!";
+                } else {
+                    echo "Failed to process, please call system administrator!";
+                }
+
+            } catch(Exception $e) {
+                echo 'Error: ' .$e->getMessage();
             }
 
-        } catch(Exception $e) {
-            echo 'Error: ' .$e->getMessage();
         }
 
     } else {
