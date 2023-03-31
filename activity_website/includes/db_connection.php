8255ea692fc184331436d0d2ea4cfc160ce176b4
DANIEL NG <danielngdxb@gmail.com>
2023-04-01 01:35:46 +0800

    a

--- activity_website/includes/db_connection.php
+++ activity_website/includes/db_connection.php
@@ -1,16 +1,45 @@
 <?php
     //REMOTE CONNECTION
-    $dbHostName = "sql947.main-hosting.eu";
-    $dbUsername = "u955154186_administrator";
-    $dbPassword = "Passw0rd";
-    $dbDatabase = "u955154186_db_djstrading";
+    // $dbHostName = "sql947.main-hosting.eu";
+    // $dbUsername = "u955154186_administrator";
+    // $dbPassword = "Passw0rd";
+    // $dbDatabase = "u955154186_db_djstrading";
 
-    // $dbConnection = mysqli_connect($dbHostName,$dbUsername,$dbPassword,$dbDatabase);
+    // $dbConnection = new mysqli ($dbHostName,$dbUsername,$dbPassword,$dbDatabase);
 
-    $dbConnection = mysqli_connect("sql947.main-hosting.eu","u955154186_administrator","Passw0rd","u955154186_db_djstrading");
+    // $dbConnection = new mysqli("sql947.main-hosting.eu","u955154186_administrator","Passw0rd","u955154186_db_djstrading");
 
-    if ($dbConnection == true) {
-        echo "Successfully Connected to REMOTE DATABASE!";
-    } else {
-        die ("Connection Failed to REMOTE DATABASE:" . $dbConnection.connect_error);
-    }
+    // if ($dbConnection == true) {
+    //     echo "Successfully Connected to REMOTE DATABASE!";
+    // } else {
+    //     die ("Connection Failed to REMOTE DATABASE:");
+    // }
+
+      //LOCAL CONNECTION
+    //   define ('DBLOCALHOST', 'localhost');
+    //   define ('DBLOCALUSER', 'root');
+    //   define ('DBLOCALPASS', '');
+    //   define ('DBLOCALDATABASE', 'db_tripledee');
+      //REMOTE CONNECTION
+    //   define ('DBREMOTEHOST', 'sql947.main-hosting.eu');
+    //   define ('DBREMOTEUSER', 'u955154186_admin');
+    //   define ('DBREMOTEPASS', 'Passw0rd');
+    //   define ('DBREMOTEDATABASE', 'u955154186_danielng');
+  
+
+  
+      $dbHost = "sql947.main-hosting.eu";
+    //   USING IP ADDRESS AS SERVER HOSTNAME
+    //   $dbHost = "89.117.188.103";
+      $dbUser = "u955154186_admin";
+      $dbPass = "Passw0rd";
+      $dbDatabase = "u955154186_danielng";
+  
+      $dbConnection = new mysqli($dbHost,$dbUser,$dbPass,$dbDatabase);
+    //   $dbConnection = new mysqli(DBREMOTEHOST,DBREMOTEUSER,DBREMOTEPASS,DBREMOTEDATABASE);
+  
+      if ($dbConnection->connect_error) {
+          die ("Connection Failed to REMOTE DATABASE:");
+      } else {
+          echo "Successfully Connected to REMOTE DATABASE!";
+      }
