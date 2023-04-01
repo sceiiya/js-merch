cd450d42e4d38ba4b2a992961d31c260e4077678
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-04-01 11:31:06 +0800

    admin script added

--- activity_website/admin/listing/index.php
+++ activity_website/admin/listing/index.php
@@ -6,78 +6,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="/activity_website/assets/css/styles.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <title>Product Listing</title>
 </head>
 
 <body class="">
 
 <main class="">
-
-<div class="modal" id="myModal">
+<button id="addProduct">List Product</button>
+<div class="modal" id="addtoListModal">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content bg-transparent border-0">
 
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">EDIT</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </div>
+      <form class="animazooki-log-cont txtc bg-light-in">
 
-      <!-- Modal body -->
-      <div class="modal-body">
-      <input id="indexer" style="display: none;">
-                        <div class="row">
-                          <div class="col-md-6 mb-4">
+        <h3 class="txtc mb-4">Log in</h3>
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+          <input type="email" id="form2Example1" class="form-control" placeholder="Email" />
+        </div>
 
-                            <div class="form-outline">
-                              <input type="text" id="firstName" name="firstName" class="form-control form-control-lg" />
-                              <label class="form-label" for="firstName">First Name</label>
-                            </div>
+        <!-- Password input -->
+        <div class="form-outline mb-0">
+          <input type="password" id="form2Example2" class="form-control" placeholder="Password" />
+        </div>
 
-                          </div>
-                          <div class="col-md-6 mb-4">
+        <!-- 2 column grid layout for inline styling -->
+        <div class="row mb-1">
+          <div class="col d-flex justify-content-center">
+            <!-- Checkbox -->
+            <div class="form-check">
+              <input class="form-check-input inbg-red" type="checkbox" value="" id="form2Example31" checked />
+              <label class="form-check-label cb-sgn-txt mt-0" for="form2Example31"> Remember me </label>
+            </div>
+          </div>
 
-                            <div class="form-outline">
-                              <input type="text" id="lastName" name="lastName" class="form-control form-control-lg" />
-                              <label class="form-label" for="lastName">Last Name</label>
-                            </div>
+          <div class="col">
+            <!-- Simple link -->
+            <a href="#!" class="txtred cb-sgn-txt mt-0" onclick="popdev()">Forgot password?</a>
+          </div>
+        </div>
 
-                          </div>
-                        </div>
+        <!-- Submit button -->
+        <button type="button" id="login" class="btn btn-primary btn-block mb-4 inbg-red rwflx txtc"><a
+            class="txt-light">Log in</a></button>
 
+        <!-- Register buttons -->
+        <div class="text-center">
+          <p>Not a member? <a class="txtred btn-signup">Create account</a></p>
+          <p>or sign up with:</p>
+          <button type="button" class="btn btn-link btn-floating mx-1">
+            <i class="fab fa-facebook-f txtred" onclick="popdev()"></i>
+          </button>
 
-                        <div class="row">
+          <button type="button" class="btn btn-link btn-floating mx-1">
+            <i class="fab fa-google txtred" onclick="popdev()"></i>
+          </button>
 
-                        <div class="col-md-6 mb-4 pb-2">
-
-                          <div class="form-outline">
-                            <input type="email" id="userName" name="userName" class="form-control form-control-lg" />
-                            <label class="form-label" for="userName">Username</label>
-                          </div>
-
-                          </div>
-                          
-                          <div class="col-md-6 mb-4 pb-2">
-
-                            <div class="form-outline">
-                              <input type="email" id="emailAddress" name="emailAddress" class="form-control form-control-lg" />
-                              <label class="form-label" for="emailAddress">Email</label>
-                            </div>
-
-                          </div>
-
-                        </div>
-
-      </div>
-
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <div class="">
-                          <!-- <input class="btn btn-primary btn-lg" type="submit" value="Register" /> -->
-                          <button class="btn btn-primary" id="edit">Modify</button>
-                        </div>
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-      </div>
+          <button type="button" class="btn btn-link btn-floating mx-1">
+            <i class="fab fa-twitter txtred" onclick="popdev()"></i>
+          </button>
+        </div>
+      </form>
 
     </div>
   </div>
@@ -87,6 +77,7 @@
 <!-- Pills content -->
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="/activity_website/assets/js/adminListingScripts.js"></script>
 <script src="/activity_website/assets/js/scripts.js"></script>
 
 </html>
