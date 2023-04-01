b6eb7cdf2b0453587ab5512c74da7d33138d2b4c
Jayfourth <fourthcesar@gmail.com>
2023-04-01 17:45:27 +0800

    added changepassword function

---
+++ activity_website/controllers/changepassword.php
@@ -0,0 +1,31 @@
+<?php
+
+include("../includes/db_connection.php");
+session_start();
+
+    $sUsername = $_SESSION['username'];
+
+    if ($dbConnection == true) {
+        $sPassword = $_POST['password'];
+
+        try {
+            $qUpdate = "UPDATE `u955154186_db_djstrading`.`clients` 
+                SET `ClientPassword` = '{$sPassword}'
+                WHERE `ClientUsername` = '{$sUsername}'
+            ";
+            
+            $eUpdate = mysqli_query($dbConnection, $qUpdate);
+
+            if ($eUpdate == true) {
+                echo "Password successfully saved!";                
+            } else {
+                echo "Failed to process, please call system administrator!";
+            }
+
+        } catch(Exception $e) {
+            echo "error";
+        }
+
+    } else {
+        echo "Failed to connect, please call system administrator!";
+    }
