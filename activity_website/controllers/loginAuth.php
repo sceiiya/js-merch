6093d6afec5e84c137ec8c3eb706bdd02f558234
Jayfourth <fourthcesar@gmail.com>
2023-04-01 10:45:37 +0800

    added validatio for username

--- activity_website/controllers/loginAuth.php
+++ activity_website/controllers/loginAuth.php
@@ -2,32 +2,32 @@
     include("../includes/db_connection.php");
     session_start();
 
-    if ($dbConn == true) {
+    if ($dbConnection == true) {
         $sUsername = $_POST['username'];
         $sPassword = $_POST['password'];
 
         try {
             $qStatSelect = "SELECT `ClientStatus` FROM `u955154186_db_djstrading`.`clients` WHERE `ClientUsername` = '$sUsername' AND `ClientPassword` = '$sPassword'";
-            $eStatSelect = mysqli_query($dbConn, $qStatSelect);
+            $eStatSelect = mysqli_query($dbConnection, $qStatSelect);
             $rows = mysqli_fetch_assoc($eStatSelect);
 
             if ($rows['ClientStatus'] == "OTP Verified") {
                 $qSelect = "SELECT `ClientPassword` FROM `u955154186_db_djstrading`.`clients` WHERE `ClientUsername` = '$sUsername'";
-                $eSelect = mysqli_query($dbConn, $qSelect);
+                $eSelect = mysqli_query($dbConnection, $qSelect);
                 $rows2 = mysqli_fetch_assoc($eSelect);
                 $nTotalRows = mysqli_num_rows($eSelect);
 
                 if ($nTotalRows > 0 && $rows2['ClientPassword'] == $sPassword) {
                     $_SESSION['username'] = $sUsername;    
                     echo "Login Success";
-                    mysqli_close($dbConn);
+                    mysqli_close($dbConnection);
                 }else{
                     echo "Login Failed";
-                    mysqli_close($dbConn);
+                    mysqli_close($dbConnection);
                 }
             } else {
                 echo "OTP Not Verified or incorrect credentials";
-                mysqli_close($dbConn);
+                mysqli_close($dbConnection);
             }
         } catch(Exception $e) {
             echo "error";
