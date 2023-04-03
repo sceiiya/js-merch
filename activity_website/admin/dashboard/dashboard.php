9a7fd72c0ffa5bbdd7f55725f5437365b6edcc1f
Jayfourth <fourthcesar@gmail.com>
2023-04-03 22:35:23 +0800

    debugging

--- activity_website/admin/dashboard/dashboard.php
+++ activity_website/admin/dashboard/dashboard.php
@@ -67,8 +67,8 @@
 <!-- Pills content -->
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-<script src="/activity_website/assets/js/adminListingScripts.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script> -->
+<script src="/activity_website/assets/js/adminfetch.js"></script>
 <script src="/activity_website/assets/js/scripts.js"></script>
 <script src="/activity_website/assets/js/dashboardscripts.js"></script>
 
--- activity_website/assets/js/adminListingScripts.js
+++ activity_website/assets/js/adminListingScripts.js
@@ -1,119 +1,72 @@
 // ==================== ADMIN isting modal
 
 $('#addProduct').on('click', () => {
+  var iCode = $("#pCode").val("");
+  var iName = $("#pName").val("");
+  var iPrice = $("#pPrice").val("");
+  var iQty = $("#pQty").val("");
+  var iDescription = $("#pDescription").val("");
+  var iImage = $("#pImage").val("");
   $('#addtoListModal').modal('show');
-});
 
-$("#ListProduct").on('click', () => {
-  var iCode = $("#pCode").val();
-  var iName = $("#pName").val();
-  var iPrice = $("#pPrice").val();
-  var iQty = $("#pQty").val();
-  var iDescription = $("#pDescription").val();
-  var iImage = $("#pImage").prop('files')[0];
-//image still in develoipmeneent
-
-  // var sJsonProduct =   new FormData();
-  // sJsonProduct.append('code', iCode);
-  // sJsonProduct.append('name', iName);
-  // sJsonProduct.append('price', iPrice);
-  // sJsonProduct.append('qty', iQty);
-  // sJsonProduct.append('description', iDescription);
-  // sJsonProduct.append('image', iImage);
-
-//   {
-//     code: iCode,
-//     name: iName,
-//     price: iPrice,
-//     qty: iQty,
-//     description: iDescription,
-//     image: iImage,
-// };
-
-var sJsonProduct = {
-      code: iCode,
-      name: iName,
-      price: iPrice,
-      qty: iQty,
-      description: iDescription,
-      image: iImage,
-  };
-
-  $.ajax({
-      type: 'POST',
-      url: "/activity_website/controllers/admin/admin_list_product.php",
-      data: sJsonProduct,
-      success: (result) => {
-          if( result == "Product Listed!") {
-            fetch();
-            $('#addtoListModal').modal('hide');
-        
-            console.log(result);
-
-              // window.location = "/activity_website/controllers/OTP.php";
-          } else if ( result == "Incomplete product description") {
-              alert("Please fill out product all details");
-          } else if ( result == "Failed Listing!") {
-              alert("Failed Listing!");
-          }else {
+  $("#ListProduct").on('click', () => {
+    iCode = $("#pCode").val();
+    iName = $("#pName").val();
+    iPrice = $("#pPrice").val();
+    iQty = $("#pQty").val();
+    iDescription = $("#pDescription").val();
+    iImage = $("#pImage").val();
+  //image still in develoipmeneent
+  
+    // var sJsonProduct =   new FormData();
+    // sJsonProduct.append('code', iCode);
+    // sJsonProduct.append('name', iName);
+    // sJsonProduct.append('price', iPrice);
+    // sJsonProduct.append('qty', iQty);
+    // sJsonProduct.append('description', iDescription);
+    // sJsonProduct.append('image', iImage);
+  
+  //   {
+  //     code: iCode,
+  //     name: iName,
+  //     price: iPrice,
+  //     qty: iQty,
+  //     description: iDescription,
+  //     image: iImage,
+  // };
+  
+  var sJsonProduct = {
+        code: iCode,
+        name: iName,
+        price: iPrice,
+        qty: iQty,
+        description: iDescription,
+        image: iImage,
+    };
+  
+    $.ajax({
+        type: 'POST',
+        url: "/activity_website/controllers/admin/admin_list_product.php",
+        data: sJsonProduct,
+        success: (result) => {
+            if( result == "Product Listed!") {
+              $('#addtoListModal').modal('hide');
               console.log(result);
-          }  
-      }
-  });
-
-});
-
-$( document ).ready( fetch()
-
-//   () => {
-
-//   $.ajax({
-//     type: 'POST',
-//     url: "/activity_website/controllers/admin_listing_fetch.php",
-//     success: (result) => {
-//         if( result == "Record successfully saved!") {
-
-//         } else if ( result == "Incomplete product description") {
-//             alert("Please fill out product all details");
-//         } else {
-//           $("").innerHTML = result;
-//         }  
-//     }
-// });
-
-// }
-
-);
-
-// function adminspinshow(){
-//   $('.sppinnermodal').modal('show');
-// }
-// function adminspinhide(){
-//   $('.sppinnermodal').modal('show');
-// }
-
-function fetch() {
-
-$.ajax({
-    type: 'POST',
-    url: "/activity_website/controllers/admin/admin_listing_fetch.php",
-    beforeSend: function (){
-      var x = document.querySelector('.sppinnermodal');
-      if (x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
-      }
-    },
-    success: (result) => {
-        if (result == "error") {
-            alert("Please call system admnistrator");
-        } else {
-            $("#admin_listedProducts").html(result);
+              fetch();
+                // window.location = "/activity_website/controllers/OTP.php";
+            } else if ( result == "Incomplete product description") {
+                alert("Please fill out product all details");
+            } else if ( result == "Failed Listing!") {
+                alert("Failed Listing!");
+            }else {
+                console.log(result);
+            }  
         }
-    },
-    // complete: function(){
-    //   document.querySelector('sppinnermodal').modal('show');
-    // },
+    });
+  
+  });
+  
 });
-}
+
+
+
