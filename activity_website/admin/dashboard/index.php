d26ed1b33220029e7942ca19604cab49e777fa91
sceiiya <meetscheidj@gmail.com>
2023-04-03 05:45:02 -0400

    modified/fixed something again in the admin side

--- activity_website/admin/dashboard/index.php
+++ activity_website/admin/dashboard/index.php
@@ -12,8 +12,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="/activity_website/assets/css/styles.css">
     <title>Admin access</title>
+</head> -->
+<head>
+<title>Dashboard</title>
 </head>
-
+<!--
 <body class="admin_dashboard">
 
 <nav class="adminNav">
