be8ebb7b39ea416a7409c718e807c4f64a3dec98
Jayfourth <fourthcesar@gmail.com>
2023-04-06 08:16:16 +0800

    updated quantity data upload to db

--- activity_website/assets/js/fetch.js
+++ activity_website/assets/js/fetch.js
@@ -52,6 +52,7 @@ function cartfetch() {
                 var productPrice = JsonArr[i].ProductPrice;
                 var productPhoto = JsonArr[i].ProductPhoto;
                 var productQuantity = JsonArr[i].ProductQuantity;
+                // var clientID = JsonArr[i].ClientId;
 
                 sHtml += `<tr>
                         <td>
@@ -59,16 +60,16 @@ function cartfetch() {
                             <div class="checkout_cart-cont"><img class="cart_item-img-main" src="/activity_website/admin/listing/product_img/${productPhoto}"></div>
 
                                 <div>
-                                    <p>${productName}</p>
+                                    <p id="pNameInfo${i}">${productName}</p>
                                     <small>${productDesc}</small>
                                     <br>
-                                    <small>₱ ${productPrice.toLocaleString()}</small>
+                                    <small id="pPriceInfo${i}">₱ ${productPrice.toLocaleString()}</small>
                                     <br>
                                     <a href="#!" onclick="removeFromCart(${JsonArr[i].CartId})">Remove</a>
                                 </div>
                             </div>
                         </td>
-                        <td><input type="number" class="inputforcart" id="input${i}" value="1" min="1" max="${productQuantity}"></td>
+                        <td><input type="number" class="inputforcart" id="input${i}" value="1" min="1" max="${productQuantity}" oninput="valueCalc(${i})"></td>
                         <td id="subinput${i}">₱ ${productPrice.toLocaleString()}</td>
                     </tr>
                     `;
@@ -102,75 +103,182 @@ function cartfetch() {
 
             $("#cart-cont").html(sHtml);
 
-            $('.inputforcart').on('keyup', () => {
+            // function valueCalc(cId) {
+            //     console.log(cId);
+            //     var allTotal = 0;
 
-                var allTotal = 0;
+            //     for (i = 0; i < JsonArr.length; i++) {
+            //         var price = JsonArr[i].ProductPrice;
+            //         // console.log(JsonArr[i].ProductPrice);
+            //         var quantity = document.getElementById('input' + i).value;
+            //         // console.log(quantity);
+            //         // console.log(price);
+            //         var total = price * quantity;
+            //         // console.log("total = " + total);
+            //         allTotal += total;
+            //         // console.log("alltotal = " + allTotal);
+            //     }
+            //     document.getElementById('subTotal').innerHTML = "₱ " + allTotal.toLocaleString();
+            //     document.getElementById('cartTotal').innerHTML ="₱ " +  allTotal.toLocaleString();
 
-                for (i = 0; i < JsonArr.length; i++) {
-                    var price = JsonArr[i].ProductPrice;
-                    // console.log(JsonArr[i].ProductPrice);
-                    var quantity = document.getElementById('input' + i).value;
-                    // console.log(quantity);
-                    // console.log(price);
-                    var total = price * quantity;
-                    // console.log("total = " + total);
-                    allTotal += total;
-                    // console.log("alltotal = " + allTotal);
-                }
-                document.getElementById('subTotal').innerHTML = "₱ " + allTotal.toLocaleString();
-                document.getElementById('cartTotal').innerHTML ="₱ " +  allTotal.toLocaleString();
+            //     // var subTotal = 0;
 
-                // var subTotal = 0;
+            //     for (i = 0; i < JsonArr.length; i++) {
+            //         var subprice = JsonArr[i].ProductPrice;
+            //         // console.log(JsonArr[i].ProductPrice);
+            //         var subquantity = document.getElementById('input' + i).value;
+            //         // console.log(quantity);
+            //         // console.log(price);
+            //         var subtotal = subprice * subquantity;
+            //         // console.log("total = " + total);
+            //         // allTotal += total;
+            //         // console.log("alltotal = " + allTotal);
+            //         document.getElementById('subinput' + i).innerHTML = "₱ " + subtotal.toLocaleString();
+            //     }
+            // }
 
-                for (i = 0; i < JsonArr.length; i++) {
-                    var subprice = JsonArr[i].ProductPrice;
-                    // console.log(JsonArr[i].ProductPrice);
-                    var subquantity = document.getElementById('input' + i).value;
-                    // console.log(quantity);
-                    // console.log(price);
-                    var subtotal = subprice * subquantity;
-                    // console.log("total = " + total);
-                    // allTotal += total;
-                    // console.log("alltotal = " + allTotal);
-                    document.getElementById('subinput' + i).innerHTML = "₱ " + subtotal.toLocaleString();
-                }
+            // $('.inputforcart').on('keyup', () => {
+
+            //     var allTotal = 0;
+
+            //     for (i = 0; i < JsonArr.length; i++) {
+            //         var price = JsonArr[i].ProductPrice;
+            //         // console.log(JsonArr[i].ProductPrice);
+            //         var quantity = document.getElementById('input' + i).value;
+            //         // console.log(quantity);
+            //         // console.log(price);
+            //         var total = price * quantity;
+            //         // console.log("total = " + total);
+            //         allTotal += total;
+            //         // console.log("alltotal = " + allTotal);
+            //     }
+            //     document.getElementById('subTotal').innerHTML = "₱ " + allTotal.toLocaleString();
+            //     document.getElementById('cartTotal').innerHTML ="₱ " +  allTotal.toLocaleString();
+
+            //     // var subTotal = 0;
+
+            //     for (i = 0; i < JsonArr.length; i++) {
+            //         var subprice = JsonArr[i].ProductPrice;
+            //         // console.log(JsonArr[i].ProductPrice);
+            //         var subquantity = document.getElementById('input' + i).value;
+            //         // console.log(quantity);
+            //         // console.log(price);
+            //         var subtotal = subprice * subquantity;
+            //         // console.log("total = " + total);
+            //         // allTotal += total;
+            //         // console.log("alltotal = " + allTotal);
+            //         document.getElementById('subinput' + i).innerHTML = "₱ " + subtotal.toLocaleString();
+            //     }
 
 
-            })
+            // })
 
-            $('.inputforcart').on('change', () => {
+            // $('.inputforcart').on('change', () => {
 
-                var allTotal = 0;
+            //     var allTotal = 0;
 
-                for (i = 0; i < JsonArr.length; i++) {
-                    var price = JsonArr[i].ProductPrice;
-                    console.log(JsonArr[i].ProductPrice);
-                    var quantity = document.getElementById('input' + i).value;
-                    // console.log(quantity);
-                    // console.log(price);
-                    var total = price * quantity;
-                    // console.log("total = " + total);
-                    allTotal += total;
-                    // console.log("alltotal = " + allTotal);
-                }
-                document.getElementById('subTotal').innerHTML = "₱ " + allTotal.toLocaleString();
-                document.getElementById('cartTotal').innerHTML = "₱ " + allTotal.toLocaleString();
+            //     for (i = 0; i < JsonArr.length; i++) {
+            //         var price = JsonArr[i].ProductPrice;
+            //         console.log(JsonArr[i].ProductPrice);
+            //         var quantity = document.getElementById('input' + i).value;
+            //         // console.log(quantity);
+            //         // console.log(price);
+            //         var total = price * quantity;
+            //         // console.log("total = " + total);
+            //         allTotal += total;
+            //         // console.log("alltotal = " + allTotal);
+            //     }
+            //     document.getElementById('subTotal').innerHTML = "₱ " + allTotal.toLocaleString();
+            //     document.getElementById('cartTotal').innerHTML = "₱ " + allTotal.toLocaleString();
 
-                for (i = 0; i < JsonArr.length; i++) {
-                    var subprice = JsonArr[i].ProductPrice;
-                    // console.log(JsonArr[i].ProductPrice);
-                    var subquantity = document.getElementById('input' + i).value;
-                    // console.log(quantity);
-                    // console.log(price);
-                    var subtotal = subprice * subquantity;
-                    // console.log("total = " + total);
-                    // allTotal += total;
-                    // console.log("alltotal = " + allTotal);
-                    document.getElementById('subinput' + i).innerHTML = "₱ " + subtotal.toLocaleString();
-                }
-            })
+            //     for (i = 0; i < JsonArr.length; i++) {
+            //         var subprice = JsonArr[i].ProductPrice;
+            //         // console.log(JsonArr[i].ProductPrice);
+            //         var subquantity = document.getElementById('input' + i).value;
+            //         // console.log(quantity);
+            //         // console.log(price);
+            //         var subtotal = subprice * subquantity;
+            //         // console.log("total = " + total);
+            //         // allTotal += total;
+            //         // console.log("alltotal = " + allTotal);
+            //         document.getElementById('subinput' + i).innerHTML = "₱ " + subtotal.toLocaleString();
+            //     }
+            // })
         }
     });
+}
+
+function valueCalc(nId) {
+
+    $.ajax({
+        type: 'POST',
+        url: "/activity_website/controllers/cart_fetch.php",
+        success: (result) => {
+            var dataArr = result;
+            // console.log(JSON.parse(dataArr));
+            var JsonArr = JSON.parse(dataArr);
+            // console.log(JsonArr);
+            
+            var allTotal = 0;
+
+            for (i = 0; i < JsonArr.length; i++) {
+                var price = JsonArr[i].ProductPrice;
+                // console.log(JsonArr[i].ProductPrice);
+                var quantity = document.getElementById('input' + i).value;
+                // console.log(quantity);
+                // console.log(price);
+                var total = price * quantity;
+                // console.log("total = " + total);
+                allTotal += total;
+                // console.log("alltotal = " + allTotal);
+            }
+            document.getElementById('subTotal').innerHTML = "₱ " + allTotal.toLocaleString();
+            document.getElementById('cartTotal').innerHTML ="₱ " +  allTotal.toLocaleString();
+        
+            // var subTotal = 0;
+        
+            for (i = 0; i < JsonArr.length; i++) {
+                var subprice = JsonArr[i].ProductPrice;
+                // console.log(JsonArr[i].ProductPrice);
+                var subquantity = document.getElementById('input' + i).value;
+                // console.log(quantity);
+                // console.log(price);
+                var subtotal = subprice * subquantity;
+                // console.log("total = " + total);
+                // allTotal += total;
+                // console.log("alltotal = " + allTotal);
+                document.getElementById('subinput' + i).innerHTML = "₱ " + subtotal.toLocaleString();
+            }
+        }
+    })
+            
+    var prodName = document.getElementById('pNameInfo' + nId).innerText;
+    var prodPrice = document.getElementById('pPriceInfo' + nId).innerText;
+    var inputval = document.getElementById('input' + nId).value;
+    // console.log("prod number " + nId + " is " + prodName);
+    // console.log("prod price " + nId + " is " + prodPrice);
+    // console.log("input number " + nId + " is " + inputval);
+    var data = {
+        index: nId,
+        pname: prodName,
+        pprice: parseInt(prodPrice.substring(2)),
+        pquant: parseInt(inputval),
+    };
+
+    $.ajax ({
+        type: 'POST',
+        url: '/activity_website/controllers/quantityupdate.php',
+        data: data,
+        success: (result) => {
+            if(result == "updated") {
+            console.log("Updated DB");
+            } else {
+                console.log(result);
+            }
+        }
+    })
+        
+
 }
 
 function removeFromCart(nId) {
